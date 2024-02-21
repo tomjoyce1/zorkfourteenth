fda10cc0eaa78cdd3173e04d2b6c94fc7530a191
Dawijak04 <145558391+Dawijak04@users.noreply.github.com>
2024-02-21 10:24:38 +0000

    coherent naming convention

--- Events.py
+++ Events.py
@@ -42,7 +42,7 @@ def verify_role(UserID):
 def create_event(club_id, title, description, date_, time_, venue_id):
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Events (Club_id, Title, Description, Date_, Time_, Venue_id) VALUES (?, ?, ?, ?, ?, ?)",
+    cursor.execute("INSERT INTO Events (ClubID, Title, Description, Date_, Time_, VenueID) VALUES (?, ?, ?, ?, ?, ?)",
                    (club_id, title, description, date_, time_, venue_id))
     conn.commit()
    
@@ -53,7 +53,7 @@ def create_event(club_id, title, description, date_, time_, venue_id):
 def register_for_event(event_id, user_id):
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Event_Registration (Event_id, User_id) VALUES (?, ?)", (event_id, user_id))
+    cursor.execute("INSERT INTO EventRegistration (EventID, UserID) VALUES (?, ?)", (event_id, user_id))
     conn.commit()
 
 #views              #######################################
@@ -69,7 +69,7 @@ def view_events(): #CHANGE THIS TO A VIEW AS APPOSE TO JUST SELECTING DATA FROM 
 def fetch_event_registrations(userID): #CHANGE THIS TO A VIEW AND ONLY SELECT THE RELEVANT DATA. AVOID SHOWING THE ID OF THE VENUE OR EVENT, SHOW THE NAME OF IT BY JOINING TABLES OR USING A SUB QUERY
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Event_Registration WHERE User_id = ?", (userID,))
+    cursor.execute("SELECT * FROM EventRegistration WHERE UserID = ?", (userID,))
     rows = cursor.fetchall()
     registered_events = [list(row) for row in rows]
     conn.close()
@@ -79,7 +79,7 @@ def fetch_event_registrations(userID): #CHANGE THIS TO A VIEW AND ONLY SELECT TH
 def coordinator_view_events(CoordinatorID): #AGAIN, USE THE VIEW FROM THE VIEW_EVENTS() FUNCTION AND JUST ADD A WHERE CONDITION TO SELECT THE RECORD WITH THE CORRECT COORDINATOR ID
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Events WHERE Club_id IN (SELECT Club_id FROM Clubs WHERE CoordinatorID = ?)", (CoordinatorID,))
+    cursor.execute("SELECT * FROM Events WHERE ClubID IN (SELECT ClubID FROM Clubs WHERE CoordinatorID = ?)", (CoordinatorID,))
     rows = cursor.fetchall()
     result = [list(row) for row in rows]
     conn.close()
@@ -89,7 +89,7 @@ def coordinator_view_events(CoordinatorID): #AGAIN, USE THE VIEW FROM THE VIEW_E
 def coordinator_view_events_pending(CoordinatorID):#USE THE VIEW FROM THE ABOVE FUNCTION AND JUST ADD "APPROVALSTATUS = 'PENDING'" TO THE WHERE CONDITION"
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Events WHERE Club_id IN (SELECT Club_id FROM Clubs WHERE CoordinatorID = ?) AND ApprovalStatus = 'pending'", (CoordinatorID,))
+    cursor.execute("SELECT * FROM Events WHERE ClubID IN (SELECT ClubID FROM Clubs WHERE CoordinatorID = ?) AND ApprovalStatus = 'pending'", (CoordinatorID,))
     rows = cursor.fetchall()
     result = [list(row) for row in rows]
     conn.close()
@@ -109,7 +109,7 @@ def admin_view_events(): #CREATE A VIEW FOR THIS OR ATLEAST CHANGE IT SO THAT RE
 def admin_view_events_pending():#TRY USING THE SAME VIEW FOR THE ADMIN AND COORDINATOR FUNCTIONS BUT DISTINGUISHED BETWEEN THEM BY USING THE WHERE CONDITION
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Event_Registration WHERE ApprovalStatus = 'pending'")
+    cursor.execute("SELECT * FROM EventRegistration WHERE ApprovalStatus = 'pending'")
     rows = cursor.fetchall()
     result = [list(row) for row in rows]
     return result
@@ -118,7 +118,7 @@ def admin_view_events_pending():#TRY USING THE SAME VIEW FOR THE ADMIN AND COORD
 def verify_event_registration(user_id, EventID): 
     conn = sqlite3.connect('MiniEpic.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Event_Registration WHERE User_id = ? AND Event_id = ?", (user_id, EventID))
+    cursor.execute("SELECT * FROM EventRegistration WHERE UserID = ? AND EventID = ?", (user_id, EventID))
     row = cursor.fetchone()
     conn.close()
     return row is not None
@@ -138,10 +138,10 @@ def update_event(event_id, title=None, description=None, date_=None, time_=None,
     if time_:
         update_query += " Time_=?,"
     if venue_id:
-        update_query += " Venue_id=?,"
+        update_query += " VenueID=?,"
     # Remove the last comma
     update_query = update_query[:-1]
-    update_query += " WHERE Event_id=?"
+    update_query += " WHERE EventID=?"
     update_values = []
     if title:
         update_values.append(title)
@@ -161,42 +161,42 @@ def update_event(event_id, title=None, description=None, date_=None, time_=None,
     # Function to cancel a user's registration for an event
 def cancel_event_registration(registration_id):
     conn, cursor = connect_to_database()
-    cursor.execute("DELETE FROM Event_Registration WHERE Registration_id=?", (registration_id,))
+    cursor.execute("DELETE FROM EventRegistration WHERE RegistrationID=?", (registration_id,))
     conn.commit()
     conn.close()
 
 # Function to approve a user's registration for an event
 def approve_registration(registration_id):
     conn, cursor = connect_to_database()
-    cursor.execute("UPDATE Event_Registration SET ApprovalStatus='approved' WHERE Registration_id=?", (registration_id,))
+    cursor.execute("UPDATE EventRegistration SET ApprovalStatus='approved' WHERE RegistrationID=?", (registration_id,))
     conn.commit()
     conn.close()
 
 # Function to reject a user's registration for an event
 def reject_registration(registration_id):#ONCE A REGISRTRATION IS REJECTED, IT SHOULD BE DELETED SO USE THE CANCEL_EVENT_REGISTRATION FUNCTION INTO THIS
     conn, cursor = connect_to_database()
-    cursor.execute("UPDATE Event_Registration SET ApprovalStatus='rejected' WHERE Registration_id=?", (registration_id,))
+    cursor.execute("UPDATE EventRegistration SET ApprovalStatus='rejected' WHERE RegistrationID=?", (registration_id,))
     conn.commit()
     conn.close()
 
 # Function to add a new venue to the database
 def create_venue(venue_name):
     conn, cursor = connect_to_database()
-    cursor.execute("INSERT INTO Venues (Venue_name) VALUES (?)", (venue_name,))
+    cursor.execute("INSERT INTO Venues (VenueName) VALUES (?)", (venue_name,))
     conn.commit()
     conn.close()
 
 # Function to delete a venue from the database
 def delete_venue(venue_id):
     conn, cursor = connect_to_database()
-    cursor.execute("DELETE FROM Venues WHERE Venue_id=?", (venue_id,))
+    cursor.execute("DELETE FROM Venues WHERE VenueID=?", (venue_id,))
     conn.commit()
     conn.close()
 
 # Function to update details of an existing venue
 def update_venue(venue_id, venue_name):
     conn, cursor = connect_to_database()
-    cursor.execute("UPDATE Venues SET Venue_name=? WHERE Venue_id=?", (venue_name, venue_id))
+    cursor.execute("UPDATE Venues SET VenueName=? WHERE VenueID=?", (venue_name, venue_id))
     conn.commit()
     conn.close()
 
@@ -204,14 +204,14 @@ def update_venue(venue_id, venue_name):
 # Function to delete an event from the database
 def delete_event(event_id):
     conn, cursor = connect_to_database()
-    cursor.execute("DELETE FROM Events WHERE Event_id=?", (event_id,))
+    cursor.execute("DELETE FROM Events WHERE EventID=?", (event_id,))
     conn.commit()
     conn.close()
 
 # Function to retrieve details of a specific event
 def get_event_details(event_id): 
     conn, cursor = connect_to_database()
-    cursor.execute("SELECT * FROM Events WHERE Event_id=?", (event_id,))
+    cursor.execute("SELECT * FROM Events WHERE EventID=?", (event_id,))
     event_details = cursor.fetchone()
     conn.close()
     return event_details
@@ -219,7 +219,7 @@ def get_event_details(event_id):
 # Function to retrieve all events a user is registered for
 def get_registered_events_for_user(user_id): #CHANGE THIS TO A VIEW AND DISPLAY ONLY RELEVANT DATA (E.G. SHOW VENUE NAME NOT ID)
     conn, cursor = connect_to_database()
-    cursor.execute("SELECT * FROM Event_Registration WHERE User_id=?", (user_id,))
+    cursor.execute("SELECT * FROM EventRegistration WHERE UserID=?", (user_id,))
     registered_events = cursor.fetchall()
     conn.close()
     return registered_events
@@ -227,7 +227,7 @@ def get_registered_events_for_user(user_id): #CHANGE THIS TO A VIEW AND DISPLAY 
 # Function to retrieve details of a specific venue
 def get_venue_details(venue_id):
     conn, cursor = connect_to_database()
-    cursor.execute("SELECT * FROM Venues WHERE Venue_id=?", (venue_id,))
+    cursor.execute("SELECT * FROM Venues WHERE VenueID=?", (venue_id,))
     venue_details = cursor.fetchone()
     conn.close()
     return venue_details
